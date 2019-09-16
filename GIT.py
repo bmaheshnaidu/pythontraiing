5ff8a7591ee31412f79097d13e9067a604b0230f
bmaheshnaidu <bmaheshnaidu@gmail.com>
2019-09-16 10:02:43 +0530

    added info

--- GIT.py
+++ GIT.py
@@ -1,6 +1,14 @@
 '''
 
-GIT Init command is used to initialize the local GIThud repository
-git add . command is used to add all the unstageed data to the stagged data
+git init  --- command is used to initialize the local GIThud repository
+git add .  ---command is used to add all the unstageed data to the stagged data
+note: . represents any type of the file
+git commit -- command is used to send the stagged data to local github repository.
+
+sourcetree --> remote --> add --> check default remote -->urlpath (github repository path)
+git push -- command is used to push the code from local github repository to remote github repository
+
+modify existing file and commiting the modify file..
+
 
 '''
